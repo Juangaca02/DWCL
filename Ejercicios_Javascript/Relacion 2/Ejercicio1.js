f621d7721fc86c9a7144b8aee0bb65f2ddb3e0d4
Juangaca02 <juanangarciacastro@gmail.com>
2023-10-01 23:37:16 +0200

    feat Chuwi to Nube

---
+++ Ejercicios_Javascript/Relacion 2/Ejercicio1.js
@@ -0,0 +1,6 @@
+var meses1 =
+    ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+function mostrar(meses1) {
+    for (let i = 0; i < meses1.length; i++) { alert(meses1[i]); }
+}
+mostrar(meses1);
