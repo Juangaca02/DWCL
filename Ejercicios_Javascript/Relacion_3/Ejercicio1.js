9eae8920fc7c3e0bda91adfbb95188ac3891b994
Juangaca02 <juanangarciacastro@gmail.com>
2023-10-13 19:03:19 +0200

    feat algo

---
+++ Ejercicios_Javascript/Relacion_3/Ejercicio1.js
@@ -0,0 +1,8 @@
+let dia = String( this.getDate() );
+let mes = String( this.getMonth() );
+let anio = this.getFullYear();
+
+
+
+document.write(`Estamos en el día ${dia} del mes ${mes}
+del primer|segundo|tercer cuatrimestre del año ${anio}`)
